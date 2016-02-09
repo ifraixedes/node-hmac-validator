edd81bb83be29e14b8781ee13cda56bfd3bfa45c
Ivan Fraixedes <ivan@fraixed.es>
2016-02-09 20:47:55 +0000

    Added test & implementation of the first release

---
+++ src/hmac-validator.js
@@ -0,0 +1,199 @@
+'use strict';
+
+let qs = require('querystring');
+let crypto = require('crypto');
+
+module.exports = createValidator;
+
+const encoding = 'ascii';
+
+/**
+ * Create Validator
+ *
+ * @param {Object} config - Contains the configuration parameters
+ *      All of them are pull out or assigned to default values in the first lines so take a
+ *      look which ones are required and accepted
+ * @returns {Function} which compute the digest and compare it to return true or false if they match.
+ *      Returned function accepts as parameters:
+ *         - Secret: the share secret to use to compute the digest
+ *         - Object or string: in case of string it will be parsed with querystrint to transform in an object
+ *         - [digest]: Digest to compare, not required if configuration specified `digestKeys` parameter, which
+ *              defines the key inside of the object to check which has the digest value
+ *  NOTE: Only object with value string properties are supported to check hmac digest
+ */
+function createValidator(config) {
+  checkConfig(config);
+
+  let algorithm = config.algorithm;
+  let format = config.format;
+  let excludedKeys = (config.excludedKeys instanceof Set) ? config.excludedKeys : new Set(config.excludedKeys);
+  let replacements = (config.replacements) ? compileReplacements(config.replacements) : null;
+  let keyValueLink = (typeof config.keyValueLink === 'string') ? config.keyValueLink : '=';
+  let pairsLink = (typeof config.pairsLink === 'string') ? config.pairsLink : '&';
+
+  let digestKey = null;
+
+  if (config.digestKey) {
+    digestKey = config.digestKey;
+    excludedKeys.add(config.digestKey);
+  }
+
+  return function (secret, payload, digest) {
+    let pobj = payloadToObject(payload);
+
+    if (!digest) {
+      if (!digestKey) {
+        throw new Error('Digest should be provided because not digest key was set');
+      } else {
+        digest = pobj[digestKey];
+      }
+    }
+
+    pobj = removeExcludedKeys(excludedKeys, pobj);
+
+    if (replacements) {
+      pobj = makeReplacements(replacements, pobj);
+    }
+
+    let hmac = crypto.createHmac(algorithm, secret);
+    hmac.update(getDigestMessage(pobj, keyValueLink, pairsLink), encoding);
+
+    return hmac.digest(format) === digest;
+  };
+}
+
+/**
+ * Makes basic checks on Hmac validator configuration like required parameters.
+ *
+ * @param {Object} config - The configuration used to create a new Hmac validator
+ * @throws {Error} When one of the checks fails
+ */
+function checkConfig(config) {
+  let requiredProps = ['algorithm', 'format'];
+  const errMsg = `Configuration isn't an object with the required properties: ${requiredProps.join(', ')}`;
+
+  if ((!config) || (typeof config !== 'object')) {
+    throw new Error(errMsg);
+  }
+
+  requiredProps.forEach(p => {
+    if (!config[p]) {
+      throw new Error(errMsg);
+    }
+  });
+}
+
+function payloadToObject(p) {
+  if (typeof p === 'string') {
+    return qs.parse(p, null, null, { maKeys: 0 });
+  }
+
+  return p;
+}
+
+function removeExcludedKeys(eKeys, obj) {
+  let result = {};
+
+  Object.keys(obj).forEach(k => {
+    if (eKeys.has(k)) {
+      return;
+    }
+
+    result[k] = obj[k];
+  });
+
+  return result;
+}
+
+function makeReplacements(replacements, obj) {
+  let result = {};
+  let oKeys = Object.keys(obj);
+  let rValues = replacements.values;
+  let rKeys = replacements.keys;
+
+  for (let key of oKeys) {
+    let value = charReplacer(obj[key], rValues);
+    key = charReplacer(key, rKeys);
+    result[key] = value;
+  }
+
+  return result;
+}
+
+function charReplacer(str, replChars) {
+  let result = '';
+
+  for (let c of str) {
+    let rc = replChars.find(pair => pair[0] === c);
+    result += rc === undefined ? c : rc[1];
+  }
+
+  return result;
+}
+
+function compileReplacements(confRepls) {
+  let errMsg = 'Invalid replacement: object properties for keys, values & both '
+    + 'must be exactly 1 character';
+  let keys, values;
+
+  if (confRepls.keys) {
+    keys = Object.keys(confRepls.keys).map(k => {
+      if (k.length !== 1) {
+        throw new Error(errMsg);
+      }
+
+      return [k, confRepls.keys[k]];
+    });
+  }
+
+  if (confRepls.values) {
+    values = Object.keys(confRepls.values).map(k => {
+      if (k.length !== 1) {
+        throw new Error(errMsg);
+      }
+
+      return [k, confRepls.values[k]];
+    });
+  }
+
+  if (confRepls.both) {
+    if (!keys) {
+      keys = [];
+    }
+
+    if (!values) {
+      values = [];
+    }
+
+    let both = confRepls.both;
+    Object.keys(both).forEach(k => {
+      if (k.length !== 1) {
+        throw new Error(errMsg);
+      }
+
+      keys.push([k, both[k]]);
+      values.push([k, both[k]]);
+    });
+  }
+
+
+  if ((!keys) && (!values)) {
+    return null;
+  }
+
+  return {
+    keys: keys,
+    values: values
+  };
+}
+
+function getDigestMessage(obj, keyValueLink, pairsLink) {
+  let sortedKeys = Object.keys(obj).sort();
+  let keyVals = [];
+
+  for (let k of sortedKeys) {
+    keyVals.push(`${k}${keyValueLink}${obj[k]}`);
+  }
+
+  return keyVals.join(pairsLink);
+}
---
+++ test/hmac-validator-test.js
@@ -0,0 +1,197 @@
+'use strict';
+
+let chai = require('chai');
+let hmacValidator = require('../src/hmac-validator');
+
+let expect = chai.expect;
+
+describe('hmac validator', () => {
+  const secret = 'hush';
+  let commonConfig = {
+    replacements: {
+      both: {
+        '&': '%26',
+        '%': '%25'
+      },
+      keys: {
+        '=': '%3D',
+        '^': '5E'
+      },
+      values: {
+        '<': '#60',
+        '>': '#62'
+      }
+    },
+    excludedKeys: ['signature', 'hmac'],
+    algorithm: 'sha256',
+    format: 'hex'
+  };
+
+  it('throws an error when missing a required configuration parameters', () => {
+    expect(hmacValidator.bind(null, { format: 'hex' })).to.throw(/configuration isn't an object with the required properties:/i);
+    expect(hmacValidator.bind(null, { altorithm: 'sha256' })).to.throw(/configuration isn't an object with the required properties:/i);
+
+    expect(hmacValidator.bind(null, { algorithm: 'sha256', format: 'hex', replacements: { both: { too: 'large' } } }))
+    .to.throw(/invalid replacement: object properties for keys, values & both must be exactly 1 character/i);
+  });
+
+  it('allows to check query strings providing digest to compare', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = 'c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7';
+
+    let c = validate(
+      secret,
+      'shop=some-shop.myshopify.com&timestamp=1337178173&signature=6e39a2ea9e497af6cb806720da1f1bf3&hmac=c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7',
+      digest);
+
+    expect(c).to.be.true;
+  });
+
+  it('throws an error to check when digest is not inside of the payload and it is not provided', () => {
+    let validate = hmacValidator(commonConfig);
+
+    expect(
+      validate.bind(null, secret, 'shop=some-shop.myshopify.com&timestamp=1337178173&signature=6e39a2ea9e497af6cb806720da1f1bf3&hmac=c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7')
+    ).to.throw(/Digest should be provided because not digest key was set/);
+  });
+
+  it('allows to check query strings provding digest as a key inside of the query string', () => {
+    let validate = hmacValidator(Object.assign({},
+      commonConfig, {
+        digestKey: 'hmac'
+      }));
+
+    let c = validate(secret, 'shop=some-shop.myshopify.com&timestamp=1337178173&signature=6e39a2ea9e497af6cb806720da1f1bf3&hmac=c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7');
+    expect(c).to.be.true;
+  });
+
+  it('allows to check objects (key/value) providing a digest to compare', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = 'c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7';
+
+    let c = validate(secret, {
+      shop: 'some-shop.myshopify.com',
+      timestamp: '1337178173',
+      signature: '6e39a2ea9e497af6cb806720da1f1bf3'
+    }, digest);
+
+    expect(c).to.be.true;
+  });
+
+  it('allows to check objects (key/value) providing an INVALID digest to compare', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = 'a2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c8';
+
+    let c = validate(secret, {
+      shop: 'some-shop.myshopify.com',
+      timestamp: '1337178173',
+      signature: '6e39a2ea9e497af6cb806720da1f1bf3'
+    }, digest);
+
+    expect(c).to.be.false;
+  });
+
+  it('allows to check objects (key/value) providing a digest as key', () => {
+    let validate = hmacValidator(
+      Object.assign({}, commonConfig, { digestKey: 'hmac' }));
+
+    let c = validate(secret, {
+      shop: 'some-shop.myshopify.com',
+      timestamp: '1337178173',
+      signature: '6e39a2ea9e497af6cb806720da1f1bf3',
+      hmac: 'c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7'
+    });
+
+    expect(c).to.be.true;
+  });
+
+  it('allows to check objects (key/value) providing a digest as key which is not included as excluded key', () => {
+    let validate = hmacValidator(
+      Object.assign({}, commonConfig, { digestKey: 'hmac', excludedKeys: ['signature'] }));
+
+    let c = validate(secret, {
+      shop: 'some-shop.myshopify.com',
+      timestamp: '1337178173',
+      signature: '6e39a2ea9e497af6cb806720da1f1bf3',
+      hmac: 'c2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c7'
+    });
+
+    expect(c).to.be.true;
+  });
+
+  it('allows to check objects (key/value) providing an INVALID digest as key', () => {
+    let validate = hmacValidator(
+      Object.assign({}, commonConfig, { digestKey: 'hmac' }));
+
+    let c = validate(secret, {
+      shop: 'some-shop.myshopify.com',
+      timestamp: '1337178173',
+      signature: '6e39a2ea9e497af6cb806720da1f1bf3',
+      hmac: 'a2812f39f84c32c2edaded339a1388abc9829babf351b684ab797f04cd94d4c8'
+    });
+
+    expect(c).to.be.false;
+  });
+
+  it('allows to replace certain characters from keys', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = '3328f4347728f5c0ed778c3557ac68e9b1b7d88d0154fc1a3cdf3cb3ee52c309';
+
+    let c = validate(
+      secret,
+      'shop=some-shop.myshopify.com&time%stamp=1337178173&sign^ature=6e39a2ea9e497af6cb806720da1f1bf3',
+      digest);
+
+    expect(c).to.be.true;
+  });
+
+  it('allows to replace certain characters from values', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = '262d203d7dbf52b26d079691e286318f6b9c53fb41994f396ce84ff4cbb43b34';
+
+    let c = validate(
+      secret,
+      'shop=some-shop.myshopify.com&timestamp=1337<17>8173&signature=6e39a2ea9e497af6cb806720da1f1bf3',
+      digest);
+
+    expect(c).to.be.true;
+  });
+
+  it('allows to replace certain characters from keys & values', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = 'b915e8f59ddd5a64fb03619796fde51f3b049de6e68e2b334d3fe386e64b9969';
+
+    let c = validate(
+      secret,
+      'shop=some-shop.myshopify.com&time%stamp=1337<17>8173&signature=6e39a2ea9e497af6cb806720da1f1bf3',
+      digest);
+
+    expect(c).to.be.true;
+  });
+
+  it('makes replacements without replacing previous replacements', () => {
+    let validate = hmacValidator(
+      Object.assign({}, commonConfig, { digestKey: 'hmac' }));
+
+    let c = validate(secret, {
+      'sh=op': 'some-shop.myshopify.com',
+      timestamp: '&1337178173',
+      hmac: '81d1af737a90ad690042ffa0ad92cbebb98ae38ba2892fcb7a87fe61faf11947'
+    });
+
+    // Expected to generate this payload to after calculate the HMAC: sh%3Dop=some-shop.myshopify.com&timestamp=%261337178173
+    expect(c).to.be.true;
+  });
+
+  it('throws an error if secret is not provided', () => {
+    let validate = hmacValidator(commonConfig);
+    let digest = '262d203d7dbf52b26d079691e286318f6b9c53fb41994f396ce84ff4cbb43b34';
+
+    let c = validate.bind(null,
+      undefined,
+      'shop=some-shop.myshopify.com&timestamp=1337<17>8173&signature=6e39a2ea9e497af6cb806720da1f1bf3',
+      digest);
+
+    expect(c).to.throw(Error);
+  });
+});
